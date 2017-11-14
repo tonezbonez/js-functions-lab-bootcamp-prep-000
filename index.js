7335e00accde5a365814fe994d4e01cceb62e740
Anthony Bracewell <anthonybracewell.violinist@gmail.com>
2017-11-14 19:45:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ function happyHolidays(){
   return 'Happy holidays!'
 }
 happyHolidays()
+
 function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`
 }
 happyHolidaysTo('you')
+
 function happyHolidaysTo(holiday, name){
 return `Happy ${holiday}, ${name}!`
 }
