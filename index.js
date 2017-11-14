01b31b1dfbc5f0d74529b0d1d847a6e5a6859999
Anthony Bracewell <anthonybracewell.violinist@gmail.com>
2017-11-14 19:43:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 // write your code below!
 function happyHolidays(){
   return 'Happy holidays!'
-};
-
+}
+happyHolidays()
 function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`
-};
-
+}
+happyHolidaysTo('you')
 function happyHolidaysTo(holiday, name){
 return `Happy ${holiday}, ${name}!`
-};
-
+}
+happyHolidaysTo('Easter', 'jesus')
 function holidayCountdown(days, holiday){
 return `It's ${days} days until ${holiday}!`
 }
+holidayCountdown(100, 'Christmas')
